019b714ec4bf5517ffa1c4100c0ed5992d46cf38
Prajwol189 <mprajwol518@gmail.com>
2024-07-02 09:27:05 +0545

    Section1:Part 2 javascript Fundamental-Functions

--- 01-Fundamentals-Part-1/Codetest/test.js
+++ 01-Fundamentals-Part-1/Codetest/test.js
@@ -1,14 +1,6 @@
-let markMass, johnMass, markHeight, johnHeight;
-markMass = 78;
-markHeight = 1.69;
-johnMass = 92;
-johnHeight = 1.95;
-//Bmi formula
-let markBMI, johnBMI;
-markBMI = markMass / markHeight ** 2;
-johnBMI = johnMass / johnHeight ** 2;
-if (markBMI > johnBMI) {
-  console.log(`MarkBMI ${markBMI} is higher than john's! ${johnBMI}`);
-} else {
-  console.log(`johnBMI is higher than marks!`);
+const bill = "275";
+const tip = prompt(`Your bill is ${bill}. How much you want to tips??`);
+
+if ((tip) => 50 && tip <= 300) {
+  console.log(`print higher tip`);
 }
