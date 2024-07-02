12ca4f4bb24a187b5054ed4da50adb4a7242155b
Prajwol189 <mprajwol518@gmail.com>
2024-07-02 21:39:06 +0545

    Section1: Basic array

--- 01-Fundamentals-Part-1/Codetest/test.js
+++ 01-Fundamentals-Part-1/Codetest/test.js
@@ -1,6 +1,18 @@
-const bill = "275";
-const tip = prompt(`Your bill is ${bill}. How much you want to tips??`);
+"use strict";
+const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-if ((tip) => 50 && tip <= 300) {
-  console.log(`print higher tip`);
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+console.log(`${scoreDolphins} socre k ${scoreKoalas}`);
+function checkWinner(avgDolhins, avgKoalas) {
+  if (avgDolhins === avgKoalas) {
+    console.log("no one wins");
+  } else if (avgDolhins >= 2 * avgKoalas) {
+    console.log("dolphins win");
+  } else if (avgKoalas >= 2 * avgDolhins) {
+    console.log("koalas win");
+  } else {
+    console.log("no one wins");
+  }
 }
+checkWinner(scoreDolphins, scoreKoalas);
