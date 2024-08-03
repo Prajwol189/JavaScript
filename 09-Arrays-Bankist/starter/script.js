decc4a8e9292ce0ce77d80a0ebb9e9297d798da9
Prajwol189 <mprajwol518@gmail.com>
2024-08-03 21:54:46 +0545

    first change

--- 09-Arrays-Bankist/starter/script.js
+++ 09-Arrays-Bankist/starter/script.js
@@ -4,6 +4,7 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -35,6 +36,7 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+/////////////////////////////////////////////////
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -62,15 +64,25 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+// Functions
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
-/////////////////////////////////////////////////
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
