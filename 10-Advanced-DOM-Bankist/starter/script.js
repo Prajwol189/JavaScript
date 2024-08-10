3a5a0f7eac00b6c7924ff51a53de29bb40381741
Prajwol Maharjan <125292726+Prajwol189@users.noreply.github.com>
2024-08-10 23:43:22 +0545

    attributes

--- 10-Advanced-DOM-Bankist/starter/script.js
+++ 10-Advanced-DOM-Bankist/starter/script.js
@@ -28,34 +28,75 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-//selecting element
+
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+/*
+///////////////////////////////////////
+// Selecting, Creating, and Deleting Elements
+
+// Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-//selecting
 const header = document.querySelector('.header');
-const allSection = document.querySelectorAll('section');
-console.log(allSection);
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
 
 document.getElementById('section--1');
-const allButton = document.getElementsByTagName('button');
-console.log(allButton);
-console.log(document.getElementsByName('btn'));
-//creating
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
 const message = document.createElement('div');
 message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
 message.innerHTML =
-  'give me cookiees<button class="btn--close-cookie">got it</button>';
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-header.prepend(message);
+// header.prepend(message);
 header.append(message);
+// header.append(message.cloneNode(true));
 
-//deleting element
+// header.before(message);
+// header.after(message);
+
+// Delete elements
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
-    message.remove();
+    // message.remove();
+    message.parentElement.removeChild(message);
   });
 
-//style
+  
+///////////////////////////////////////
+// Styles, Attributes and Classes
+  
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
