a2f04aea3575b9a714f9f5afb9f03d1da59ffbf0
Prajwol Maharjan <125292726+Prajwol189@users.noreply.github.com>
2024-08-13 22:00:30 +0545

    types

--- 10-Advanced-DOM-Bankist/starter/script.js
+++ 10-Advanced-DOM-Bankist/starter/script.js
@@ -29,7 +29,6 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
@@ -116,3 +115,19 @@ logo.classList.add('c', 'j');
 logo.classList.remove('c', 'j');
 logo.classList.toggle('c');
 logo.classList.contains('c'); // not includes
+///////////////////////////////////////
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+*/
