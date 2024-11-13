76d608d1961f712eb7c20d6e509dba5905cf282c
Prajwol Maharjan <prajwolhh@gmail.com>
2024-11-13 20:29:29 +0545

    change

--- 11-Asynchronous/starter/script.js
+++ 11-Asynchronous/starter/script.js
@@ -4,3 +4,13 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// Our First AJAX Call: XMLHttpRequest
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
