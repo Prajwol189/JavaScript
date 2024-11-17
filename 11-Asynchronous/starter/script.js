c600b8fc0f5e44eabfa512255eade880e8fa59d6
Prajwol Maharjan <prajwolhh@gmail.com>
2024-11-17 21:38:41 +0545

    trying public api

--- 11-Asynchronous/starter/script.js
+++ 11-Asynchronous/starter/script.js
@@ -3,6 +3,41 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText)[0]; // Access the first object in the array
+    console.log(data);
+
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} million people</p>
+          <p class="country__row"><span>🗣️</span>${Object.values(
+            data.languages
+          ).join(', ')}</p>
+          <p class="country__row"><span>💰</span>${
+            Object.values(data.currencies)[0].name
+          }</p>
+        </div>
+      </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+// Test the function with a country
+getCountryData('nepal');
+
 /*
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
@@ -74,3 +109,4 @@ const getCountryAndNeighbour = function (country) {
   });
 };
 
+*/
